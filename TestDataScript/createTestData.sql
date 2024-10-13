f7a5ae623f00d0332680470ea235c9ab4ac2972d
spoon122 <spoon122@DESKTOP-V8S8IL9>
2024-10-13 23:31:56 +0300

    Added createTestData.sql to version control

---
+++ TestDataScript/createTestData.sql
@@ -0,0 +1,23 @@
+﻿DO $$
+    DECLARE
+        NumberOfTrades INT := 100;
+        OpenPrice FLOAT;
+        OpenTime TIMESTAMP;
+    BEGIN
+        FOR i IN 1..NumberOfTrades LOOP
+                OpenPrice := round((random() * (100 - 10) + 10)::numeric, 2);
+
+                OpenTime := NOW() - INTERVAL '1 hour' * (random() * (24 - 8) + 8);
+
+                INSERT INTO trades_table ("Ticker", "Side", "OpenPrice", "ClosePrice", "Quantity", "OpenTime", "CloseTime")
+                VALUES (
+                           (ARRAY['Ticker1', 'Ticker2', 'Ticker3', 'Ticker4', 'Ticker5'])[floor(random() * 5 + 1)],
+                           (ARRAY['BUY', 'SELL'])[floor(random() * 2 + 1)],
+                           OpenPrice,
+                           round((OpenPrice * (1 + (random() * 20 - 10) / 100))::numeric, 2), 
+                           round((random() * (25 - 1) + 1)::numeric, 2),
+                           OpenTime,
+                           OpenTime + INTERVAL '1 hour' * (random() * 3 + 1)
+                       );
+            END LOOP;
+    END $$;
