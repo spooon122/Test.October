9a47f92c0a3030883bc30c0843051b299408b84e
spoon122 <spoon122@DESKTOP-V8S8IL9>
2024-10-14 01:30:57 +0300

    Added TradesStats.cshtml for get visual stats with razor pages

---
+++ Test.October/Pages/TradesStats.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Test.October.Data;
+using Test.October.Data.Models;
+
+namespace Test.October.Pages;
+
+public class TradesStats(TradeDbContext db) : PageModel
+{
+    public List<Trade> Trades { get; set; } = [];
+    public async Task OnGetAsync(string? ticker)
+    {
+        Trades = !string.IsNullOrEmpty(ticker)
+            ? await db.Trade.Where(t => t.Ticker == ticker).ToListAsync()
+            : await db.Trade.ToListAsync();
+    }
+}
