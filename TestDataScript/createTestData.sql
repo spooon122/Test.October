62232c232ab480ffadfc259a7054ccb972d55b81
spoon122 <spoon122@DESKTOP-V8S8IL9>
2024-10-16 16:06:36 +0300

    Reworked docker-compose.yml and createTestData.sql for auto initial db

--- TestDataScript/createTestData.sql
+++ TestDataScript/createTestData.sql
@@ -1,23 +1,25 @@
-﻿DO $$
+﻿DO
+$$
     DECLARE
-        NumberOfTrades INT := 100;
-        OpenPrice FLOAT;
-        OpenTime TIMESTAMP;
+        NumberOfTrades INT := 10;
+        OpenPrice      FLOAT;
+        OpenTime       TIMESTAMP;
     BEGIN
-        FOR i IN 1..NumberOfTrades LOOP
+        FOR i IN 1..NumberOfTrades
+            LOOP
                 OpenPrice := round((random() * (100 - 10) + 10)::numeric, 2);
 
                 OpenTime := NOW() - INTERVAL '1 hour' * (random() * (24 - 8) + 8);
 
-                INSERT INTO trades_table ("Ticker", "Side", "OpenPrice", "ClosePrice", "Quantity", "OpenTime", "CloseTime")
-                VALUES (
-                           (ARRAY['Ticker1', 'Ticker2', 'Ticker3', 'Ticker4', 'Ticker5'])[floor(random() * 5 + 1)],
-                           (ARRAY['BUY', 'SELL'])[floor(random() * 2 + 1)],
-                           OpenPrice,
-                           round((OpenPrice * (1 + (random() * 20 - 10) / 100))::numeric, 2), 
-                           round((random() * (25 - 1) + 1)::numeric, 2),
-                           OpenTime,
-                           OpenTime + INTERVAL '1 hour' * (random() * 3 + 1)
-                       );
+                INSERT INTO trades_table ("ticker", "side", "openprice", "closeprice", "quantity", "opentime",
+                                          "closetime")
+                VALUES ((ARRAY ['Ticker1', 'Ticker2', 'Ticker3', 'Ticker4', 'Ticker5'])[floor(random() * 5 + 1)],
+                        (ARRAY ['BUY', 'SELL'])[floor(random() * 2 + 1)],
+                        OpenPrice,
+                        round((OpenPrice * (1 + (random() * 20 - 10) / 100))::numeric, 2),
+                        round((random() * (25 - 1) + 1)::numeric, 2),
+                        OpenTime,
+                        OpenTime + INTERVAL '1 hour' * (random() * 3 + 1));
             END LOOP;
-    END $$;
+    END
+$$;
